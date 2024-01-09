27fc81ca8af4208e03df1a3e693794939c3e7fc4
amithkashyap <amithkashyaprv@gmail.com>
2024-01-10 02:56:11 +0530

    updated main app to implement routing and lazy loading

--- src/App.js
+++ src/App.js
@@ -1,19 +1,63 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./pages/About";
+
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Contact from "./pages/Contact";
+import Error from "./components/Error";
+import RestaurantMenu from "./pages/RestaurantMenu";
+import Shimmer from "./components/Shimmer";
+// import Grocery from "./pages/Grocery";
+
+const Grocery = lazy(() => import("./pages/Grocery"));
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<Shimmer />}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/restaurants/:id",
+        element: <RestaurantMenu />,
+      },
+    ],
+  },
+]);
 
 const rootElement = document.getElementById("root");
 
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(<App />);
+root.render(<RouterProvider router={appRouter} />);
